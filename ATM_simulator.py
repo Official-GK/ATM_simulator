6d5a473938a69c87ae1e8e209417d79fefcc4e47
GK <gauravkul2006@gmail.com>
2024-11-30 14:55:23 +0530

    adding the check_balance option in this

--- ATM_simulator.py
+++ ATM_simulator.py
@@ -1,66 +1,80 @@
 class ATM_simulator:
     def __init__(self):
-        self.users=[["user1",2020,5000],["user2",2024,10000]]
-        self.user_varaible=0
-        self.l=0
-    def sign_in(self):
-        a=input("enter your username")
-        for i in range(len(self.users)):
-            self.user_varaible+=1
-            if a in self.users[i]:
-                print("you enter corect usename")
-                self.l+=1
-                b=int(input("now enter pin to proceed"))
-                if b==self.users[i][1]:
-                    print("you enterd correct pin")
-                    self.l+=1
-                    break
-                else:
-                    print("wrong pin")
-                    break
-        else:
-            print("usename is not present in our system")
-    def withdraw_money(self):
-        if self.l==2:
-            c=int(input("how much money you want to withdraw"))
-            if c<=self.users[self.user_varaible-1][2]:
-                print("your money is withdrawing")
-                self.users[self.user_varaible-1][2]-=c
-            else:
-                print("you dont have this much of balance")
-        else:
-            print("do sign in first")
-    def deposit_amount(self):
-        if self.l==2:
-            d=int(input("enter how much money you want to add to your balance"))
-            self.users[self.user_varaible-1][2]+=d
-            print("successfully added money")
-        else:
-            print("do sign in first")
-    def change_pin(self):
-        if self.l==2:
-            e=int(input("enter new pin "))
-            self.users[self.user_varaible-1][1]=e
-            print("your pin change successfult")
-        else:
-            print("do sign in first")
+        self.users = [["user1", 2020, 5000], ["user2", 2024, 10000]]
+        self.user_index = -1
+        self.logged_in = False
 
-s=ATM_simulator()
+    def sign_in(self):
+        username = input("Enter your username: ")
+        for i, user in enumerate(self.users):
+            if username == user[0]:
+                pin = int(input("Enter your PIN: "))
+                if pin == user[1]:
+                    print("Login successful.")
+                    self.user_index = i
+                    self.logged_in = True
+                    return
+                else:
+                    print("Incorrect PIN.")
+                    return
+        print("Username not found in our system.")
+
+    def check_balance(self):
+        if self.logged_in:
+            print(f"Your current balance is: ${self.users[self.user_index][2]}")
+        else:
+            print("Please sign in first.")
+
+    def withdraw_money(self):
+        if self.logged_in:
+            amount = int(input("How much money do you want to withdraw? "))
+            if amount <= self.users[self.user_index][2]:
+                print("Your money is being withdrawn.")
+                self.users[self.user_index][2] -= amount
+                print(f"New balance: ${self.users[self.user_index][2]}")
+            else:
+                print("Insufficient funds.")
+        else:
+            print("Please sign in first.")
+
+    def deposit_amount(self):
+        if self.logged_in:
+            amount = int(input("Enter how much money you want to deposit: "))
+            self.users[self.user_index][2] += amount
+            print("Successfully added money.")
+            print(f"New balance: ${self.users[self.user_index][2]}")
+        else:
+            print("Please sign in first.")
+
+    def change_pin(self):
+        if self.logged_in:
+            new_pin = int(input("Enter new PIN: "))
+            self.users[self.user_index][1] = new_pin
+            print("Your PIN has been changed successfully.")
+        else:
+            print("Please sign in first.")
+
+s = ATM_simulator()
 s.sign_in()
+
 while True:
-    print("\n1. Withdraw Money") 
-    print("\n2. Deposit Amount") 
-    print("\n3. Change PIN") 
-    print("\n4. Exit") 
+    print("\n1. Check Balance")
+    print("2. Withdraw Money")
+    print("3. Deposit Amount")
+    print("4. Change PIN")
+    print("5. Exit")
     choice = int(input("Enter your choice: "))
+    
     if choice == 1:
-        s.withdraw_money() 
+        s.check_balance()
     elif choice == 2:
-        s.deposit_amount()
+        s.withdraw_money()
     elif choice == 3:
-        s.change_pin() 
+        s.deposit_amount()
     elif choice == 4:
-         print("Exiting. Thank you for using the ATM!") 
-         break 
-    else: 
-         print("Invalid choice. Please try again.")
+        s.change_pin()
+    elif choice == 5:
+        print("Exiting. Thank you for using the ATM!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
